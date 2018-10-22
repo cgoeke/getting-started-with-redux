20f601411de710388dcb41e7e40f49b17d0e2931
Christian Goeke <christianpgoeke@gmail.com>
2018-10-22 14:18:25 -0700

    react counter example

--- src/index.js
+++ src/index.js
@@ -1,2 +1,76 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+
+// Implements Redux createStore from scratch
+// const createStore = (reducer) => {
+//   let state;
+//   let listeners = [];
+
+//   const getState = () => state;
+
+//   const dispatch = (action) => {
+//     state = reducer(state, action);
+//     listeners.forEach(listener => listener());
+//   };
+
+//   const subscribe = (listener) => {
+//     listeners.push(listener);
+//     return () => { // Returns function to unsubscribe listener
+//       listeners = listeners.filter(l => l !== listener);
+//     };
+//   };
+
+//   dispatch({}); // Sets initial value
+
+//   return { getState, dispatch, subscribe };
+// }
+
+
+// Reducer funtion
+const counter = (state = 0, action) => {
+  switch(action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+}
+
+// Presentational Component
+const Counter = ({ value, onIncrement, onDecrement }) => (
+  <div>
+    <h1>{value}</h1>
+    <button onClick={onIncrement}>+</button>
+    <button onClick={onDecrement}>-</button>
+  </div>
+);
+
+// Initiates Redux store passing in counter reducer
+const store = createStore(counter);
+
+// Renders Counter component to DOM passing in state and props
+const render = () => {
+  ReactDOM.render(
+    <Counter
+      value={store.getState()}
+      onIncrement={() =>
+        store.dispatch({
+          type: 'INCREMENT'
+        })
+      }
+      onDecrement={() =>
+        store.dispatch({
+          type: 'DECREMENT'
+        })
+      } 
+    />,
+    document.getElementById('root')
+  );
+}
+
+// Re-renders on state change
+store.subscribe(render);
+render();
