d316b1aea91435cfd992385f4ad46e9496fa1f28
Christian Goeke <christianpgoeke@gmail.com>
2018-10-23 14:01:38 -0700

    refactors entry point

---
+++ src/configureStore.js
@@ -0,0 +1,42 @@
+import { createStore } from 'redux';
+import { loadState, saveState } from './localStorage';
+import { todoApp } from './reducers/reducers';
+// Implements Redux createStore from scratch
+// const createStore = (reducer) => {
+//   let state;
+//   let listeners = [];
+
+//   const getState = () => state;
+
+//   const dispatch = (action) => {
+//     state = reducer(state, action);
+//     listeners.forEach(listener => listener());
+//   };
+
+//   const subscribe = (listener) => {
+//     listeners.push(listener);
+//     return () => { // Returns function to unsubscribe listener
+//       listeners = listeners.filter(l => l !== listener);
+//     };
+//   };
+
+//   dispatch({}); // Sets initial value
+
+//   return { getState, dispatch, subscribe };
+// }
+const configureStore = () => {
+  // Checks local storage for persisted state
+  const persistedState = loadState();
+  // Initiates Redux store passing in the root reducer
+  const store = createStore(todoApp, persistedState);
+  // Saves current app state to local storage
+  store.subscribe(() => {
+    saveState({
+      todos: store.getState().todos
+    });
+  });
+
+  return store;
+};
+
+export default configureStore;
--- src/index.js
+++ src/index.js
@@ -1,59 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import { loadState, saveState } from './localStorage';
-import { todoApp } from './reducers/reducers';
-import VisibleTodoList from './components/VisibleTodoList';
-import AddTodo from './components/AddTodo';
-import Footer from './components/Footer';
+import configureStore from './configureStore';
+import Root from './components/Root';
 
-// Implements Redux createStore from scratch
-// const createStore = (reducer) => {
-//   let state;
-//   let listeners = [];
-
-//   const getState = () => state;
-
-//   const dispatch = (action) => {
-//     state = reducer(state, action);
-//     listeners.forEach(listener => listener());
-//   };
-
-//   const subscribe = (listener) => {
-//     listeners.push(listener);
-//     return () => { // Returns function to unsubscribe listener
-//       listeners = listeners.filter(l => l !== listener);
-//     };
-//   };
-
-//   dispatch({}); // Sets initial value
-
-//   return { getState, dispatch, subscribe };
-// }
-
-// Checks local storage for persisted state
-const persistedState = loadState();
-// Initiates Redux store passing in the root reducer
-const store = createStore(todoApp, persistedState);
-// Saves current app state to local storage
-store.subscribe(() => {
-  saveState({
-    todos: store.getState().todos
-  });
-});
-
-const TodoApp = () => (
-  <div>
-    <AddTodo />
-    <VisibleTodoList />
-    <Footer />
-  </div>
-);
+const store = configureStore();
 
 ReactDOM.render(
-  <Provider store={store}>
-    <TodoApp/>
-  </Provider>,
+  <Root store={store} />,
   document.getElementById('root')
 );
